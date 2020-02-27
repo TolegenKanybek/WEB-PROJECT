4323f15e81191a0fb13e1f21d55e99d084dddcbd
Nurzhan <nurzhan.marat.2000@mail.ru>
2020-02-27 18:46:13 +0600

    a

---
+++ observable.js
@@ -0,0 +1,14 @@
+class NewsPaper{
+    constructor(name){
+        this.name=name;
+    }
+    publishNews(news){
+        console.log(this.name+':'+news);
+    }
+}
+
+
+
+class FshionNewObservable{
+    
+}
---
+++ week4/my-app/src/app/product-details/product-details.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+import {products} from '../products'
+
+@Component({
+  selector: 'app-product-details',
+  templateUrl: './product-details.component.html',
+  styleUrls: ['./product-details.component.css']
+})
+export class ProductDetailsComponent implements OnInit {
+        product;
+  constructor(
+    private route:ActivatedRoute,
+    private router:Router
+   
+  ) { }
+
+  ngOnInit(){
+    this.route.paramMap.subscribe(params => {
+      this.product=products[+params.get('productId')];
+    })
+
+  }
+
+
+
+}
