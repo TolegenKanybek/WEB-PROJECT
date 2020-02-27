291a1be614c09481eca02d9def0e8d4c4b3499fb
Nurzhan <nurzhan.marat.2000@mail.ru>
2020-02-27 19:37:09 +0600

    b

---
+++ week5/my-app/src/app/categories.ts
@@ -0,0 +1,18 @@
+export const categories=[
+    {
+        categoryId:1,
+        name:"техника"
+    },
+    {
+        categoeyId:2,
+        name:"одeжда и аксессуары"
+    },
+    {
+        categoryId:3,
+        name:"игрушки"
+    },
+    {
+        categoryId:4,
+        name:"мебель"
+    }
+];
---
+++ week5/my-app/src/app/products.ts
@@ -0,0 +1,113 @@
+export const products = [
+  {
+    productId: 1,
+    categoryId:2,
+    name: 'Водонепроницаемый чехол для обуви',
+    price: 'KZT 1,080.75 - 2,346.65',
+    description: 'Многоразовые водонепроницаемые непромокаемые чехлы для обуви силиконовые моющиеся износостойкие Чехлы для обуви резиновые сапоги для взрослых детей',
+    image:'/assets/shoe.jpg',
+    rating:4.6,
+    link:'https://aliexpress.ru/item/4000274724284.html?spm=a2g0o.productlist.0.0.124d3056R5P7JO&s=p&algo_pvid=e8d61b07-9c9e-4bc8-9f3c-48d619dbabee&algo_expid=e8d61b07-9c9e-4bc8-9f3c-48d619dbabee-0&btsid=0ab6fa8115816068129226272e56b6&ws_ab_test=searchweb0_0,searchweb201602_,searchweb201603_'
+  },
+  {
+    productId:2,
+    categoryId:1,
+    name:'кофи машина',
+    price:'KZT 12,455.01',
+    description:'Японский стиль сифон кофеварка чай сифон горшок вакуумная Кофеварка стеклянный Тип кофе машина фильтр 3 чашки',
+    image:'/assets/coffee.jpg',
+    rating:4.8,
+    link:'https://aliexpress.ru/item/4000364318898.html?spm=a2g0o.productlist.0.0.79ac713c9WYx9L&s=p&algo_pvid=594fb3d6-976c-415f-a088-00bea6325b7d&algo_expid=594fb3d6-976c-415f-a088-00bea6325b7d-0&btsid=0ab6f82c15816097221771557e9bda&ws_ab_test=searchweb0_0,searchweb201602_,searchweb201603_'
+    
+  },
+  {
+    productId:3,
+    categoryId:3,
+    name:'магнитные наушники',
+    price:'KZT 423.23 - 797.34',
+    description:'Магнитные Bluetooth наушники спортивные шейные магнитные беспроводные Игровые наушники стерео наушники металлические наушники с микрофоном для IPhone',
+    image:'/assets/earpods.jpg',
+    rating:5.0,
+    link:'https://aliexpress.ru/item/4000570106654.html?spm=a2g0o.productlist.0.0.1a4e7c64v8CrmG&algo_pvid=eda342b2-1746-4425-b629-db2a2e076d89&algo_expid=eda342b2-1746-4425-b629-db2a2e076d89-8&btsid=0ab6f82415816038690353328e72a7&ws_ab_test=searchweb0_0,searchweb201602_,searchweb201603_'
+  
+  },
+  {
+    productId:4,
+    categoryId:1,
+    name:'Алмазный точильный камень',
+    price:'KZT 3.78 - 8,517.47',
+    description:'Алмазный точильный камень для KME точилка для ножей фиксированный угол точилка для ножей точильный камень Алмазный точильный камень масляный камень хонинговые камни',
+    image:'/assets/rock.jpg',
+    rating:4.9,
+    link:'https://aliexpress.ru/item/4000184292949.html?spm=a2g01.12088204.wjnka9vkfjp2jo0.1.3da6YSGPYSGPBJ&gps-id=5823166&scm=1007.21394.141905.0&scm_id=1007.21394.141905.0&scm-url=1007.21394.141905.0&pvid=010d30b5-6767-4fdb-a9e0-de18981c162e'
+  },
+  {
+    productId:5,
+    categoryId:1,
+    name:'паяльник',
+    price:'KZT 3,858.18 - 4,322.98',
+    description:'JCD 220 в 80 Вт ЖК-Электрический паяльник 908 с регулируемой температурой паяльник с качественным паяльником наконечники и наборы',
+    image:'/assets/паяльник.jpg',
+    rating:4.9,
+    link:'https://aliexpress.ru/item/33011549041.html?spm=a2g0o.productlist.0.0.6c6c3272ARAyPj&s=p&algo_pvid=1906ccce-a473-43a4-a633-5859eb8f495c&algo_expid=1906ccce-a473-43a4-a633-5859eb8f495c-1&btsid=0ab6d67915816087510103124e4a38&ws_ab_test=searchweb0_0,searchweb201602_,searchweb201603_'
+  },
+  {
+    productId:6,
+    categoryId:1,
+    name:'Планшет, ноутбук',
+    price:'KZT 65,732.63 - 93,907.54',
+    description:'Планшет, ноутбук, 11,6 дюймов, планшет на android, 2 в 1, 10 ядер, игровая пленка, музыкальные планшеты, gps, Wi-Fi, 4G, sim-карта, телефон с клавиатурой',
+    image:'/assets/tablet.jpg',
+    rating:4.8,
+    link:'https://aliexpress.ru/item/4000076988574.html?spm=a2g0o.productlist.0.0.11ea584e8f8BVm&s=p&algo_pvid=0ccb75c7-dcc0-4162-8f4d-95dd7d685a5c&algo_expid=0ccb75c7-dcc0-4162-8f4d-95dd7d685a5c-4&btsid=0ab6f8ad15816099565975033ee3a9&ws_ab_test=searchweb0_0,searchweb201602_,searchweb201603_'
+    },
+  {
+    productId:7,
+    categoryId:2,
+    name:'перчатки',
+    price:'KZT 2,263.52',
+    description:'Перчатки для тренажерного зала, тяжелая спортивная гимнастика, перчатки для тяжелой атлетики, бодибилдинг, тренировка, спорт, фитнес-перчатки для занятий велоспортом',
+    image:'/assets/gloves.jpg',
+    rating:4.6,
+    link:'https://aliexpress.ru/item/33031184597.html?spm=a2g0o.productlist.0.0.a13d7b9dLehpfc&algo_pvid=6dbaafff-9ac9-4071-b50d-c207972c4e06&algo_expid=6dbaafff-9ac9-4071-b50d-c207972c4e06-40&btsid=0ab6f82c15815988393327892e9c68&ws_ab_test=searchweb0_0,searchweb201602_,searchweb201603_'
+  },
+  {
+    id:8,
+    categoryId:2,
+    name:'прозрачная бутылка',
+    price:'KZT 763.33 - 948.49',
+    description:'Прозрачная бутылка для воды чашка, бутылка для воды Прозрачный на открытом воздухе спортивные очки для велоспорта, фруктовый сок портативная дорожная бутылка Аксессуары для велосипеда',
+    image:'/assets/bottle.jpg',
+    rating:5.0,
+    link:'https://aliexpress.ru/item/32960590641.html?spm=a2g0o.productlist.0.0.a13d7b9dLehpfc&algo_pvid=6dbaafff-9ac9-4071-b50d-c207972c4e06&algo_expid=6dbaafff-9ac9-4071-b50d-c207972c4e06-42&btsid=0ab6f82c15815988393327892e9c68&ws_ab_test=searchweb0_0,searchweb201602_,searchweb201603_'
+  },
+  {
+    id:9,
+    categoryId:1,
+    name:'камера видеонаблюдения',
+    price:'KZT 6,420.23 - 7,553.87',
+    description:'Беспроводная аудио HD 1080P 2MP IP камера WiFi 720P наружная камера видеонаблюдения для помещений Водонепроницаемая камера Onvif',
+    image:'/assets/камера.jpg',
+    rating:5.0,
+    link:'https://aliexpress.ru/item/4000587531138.html?spm=a2g0o.productlist.0.0.650e1f764VhTMb&s=p&algo_pvid=92d8d6f0-5af7-411a-a35e-75948ee2da1b&algo_expid=92d8d6f0-5af7-411a-a35e-75948ee2da1b-4&btsid=0be3769015815992246275618ef2a1&ws_ab_test=searchweb0_0,searchweb201602_,searchweb201603_'
+  },
+  {
+    id:10,
+    categoryId:1,
+    name:'ноутбук',
+    price:'KZT 143,591.50 - 196,495.02',
+    description:'VORKE ноутбук 15 ультратонкий SSD ноутбук Intel Core i7-4500U i5-8250U 15,6 экран 1920*1080 Windows 10 8 Гб DDR3 256 ГБ SSD',
+    image:'/assets/laptop.jpg',
+    rating:4.8,
+    link:'https://aliexpress.ru/item/4000260720841.html?spm=a2g0o.productlist.0.0.66da50616ltMob&s=p&algo_pvid=7cc05b88-65d4-4152-9133-8ec968373c37&algo_expid=7cc05b88-65d4-4152-9133-8ec968373c37-2&btsid=0ab6fa7b15816002430235431e1fbb&ws_ab_test=searchweb0_0,searchweb201602_,searchweb201603_'
+  }
+  
+  
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
